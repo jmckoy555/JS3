ea22f63184ada0b3158277a0d7b50897a808267a
Jordan McKoy <jordanmckoy@MacBook-Pro.local>
2020-11-13 18:17:19 -0500

    First Commit

---
+++ Easy/script.js
@@ -0,0 +1,8 @@
+function run() {
+    console.log("The exercise for today is running");
+}
+run()
+function swim() {
+    console.log("The exercise for today is swimming");
+}
+swim()
---
+++ Medium/script.js
@@ -0,0 +1,7 @@
+let x = 8;
+function sharePizza(x, y) {
+    console.log("Each person gets" + " " + x / y + " " + "slices of pizza.");
+}
+sharePizza(x, 2);
+sharePizza(x, 3);
+
